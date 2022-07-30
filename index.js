30ecba6ab1e1ffb71c4e10fc6f724f36f466b050
Танасенко Константин Владимирович <tanasenko@elewise.local>
2022-07-30 12:14:38 +0400

    reconfig flyBird

--- index.js
+++ index.js
@@ -5,37 +5,63 @@ const BACKGROUND = document.getElementById('main');
 //console.log(BACKGROUND);
 
 //Функция получения координат птички
-const getCoords = function (elem) {
-  elem = BIRD;
-  let box = elem.getBoundingClientRect();
+// const getCoords = function (elem) {
+//   elem = BIRD;
+//   let box = elem.getBoundingClientRect();
 
-  //console.log(box)
+//   //console.log(box)
 
-  return {
-    top: box.top /*+ scrollY*/,
-    bootom: box.bottom /*+ scrollY*/,
-    lef: box.left /*+ scrollX*/,
-    right: box.right /*+ screenX*/
-  }
-}
-let timerId;
+//   return {
+//     top: box.top /*+ scrollY*/,
+//     bootom: box.bottom /*+ scrollY*/,
+//     lef: box.left /*+ scrollX*/,
+//     right: box.right /*+ screenX*/
+//   }
+// }
+// let timerId;
+// //Функция падения птички
+// function goBottom() {
+//   let positionNow = getCoords()
+
+//   timerId = setTimeout(function () {
+//     x += 10
+//     BIRD.style.top = positionNow.top + x +'px';
+//     //console.log(BIRD.style.top);
+//     if (x < 290) {
+//       goBottom();
+//     }
+//     /*if (BIRD.style.top > '600px'){
+//       console.log('yay');
+//       BIRD.style.top = '300px';
+//       clearTimeout(timerId);
+//     }*/
+//   }, 300)
+// }
+
+// let x = 0;
+// goBottom();
+
+// //Реализация полета
+// const flyBird = function () {
+//   clearTimeout(timerId);
+//   let positionNow = getCoords();
+//   BIRD.style.top = positionNow.top - 100 + 'px';
+//   goBottom();
+// }
+
+// BACKGROUND.onclick = flyBird;
+
+
 //Функция падения птички
 function goBottom() {
-  let positionNow = getCoords()
-
-  timerId = setTimeout(function () {
-    x += 10
-    BIRD.style.top = positionNow.top + x +'px';
-    //console.log(BIRD.style.top);
-    if (x < 290) {
+  setTimeout(function () {
+    x += 10;
+    BIRD.style.transform = `translateY(${x}px)`;
+    if(BIRD.style.transform != 'translateY(290px)'){
       goBottom();
+      console.log('heu');
     }
-    /*if (BIRD.style.top > '600px'){
-      console.log('yay');
-      BIRD.style.top = '300px';
-      clearTimeout(timerId);
-    }*/
-  }, 300)
+  }, 250)
 }
 
 let x = 0;
